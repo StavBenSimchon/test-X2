8518f1d891d08e8cbc3dca31f5172867381d26bc
mesmeslip <49732550+mesmeslip@users.noreply.github.com>
2020-12-21 13:55:22 +0200

    test

---
+++ ci.groovy
@@ -0,0 +1,25 @@
+node {
+    stage(){
+        checkout([$class: 'GitSCM', branches: [[name: '**']], 
+        doGenerateSubmoduleConfigurations: false, 
+        extensions: [], 
+        submoduleCfg: [], 
+        userRemoteConfigs: [[refspec: '+refs/heads/develop:refs/remotes/origin/develop +refs/heads/feature-*:refs/remotes/origin/feature-*', url: 'https://github.com/mesmeslip/test-X2.git']]])
+        sh '''#! /bin/bash
+            echo "test"
+            echo $payload
+            echo $BRANCH
+            git branch
+            env
+        '''
+    }
+    stage("build"){
+        echo "build"
+    }
+    stage("test"){
+        echo "test"
+    }
+    stage("deploy"){
+        echo "deploy"
+    }
+}

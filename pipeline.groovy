47fa2418d297933b235d8fdf307fe9d001f7dbf5
mesmeslip <49732550+mesmeslip@users.noreply.github.com>
2020-12-21 13:38:24 +0200

    test - 1

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,5 +1,9 @@
 node {
-    checkout([$class: 'GitSCM', branches: [[name: '**']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[refspec: '+refs/heads/master:refs/remotes/origin/master +refs/heads/develop:refs/remotes/origin/develop +refs/heads/release-*:refs/remotes/origin/release-* +refs/heads/feature-*:refs/remotes/origin/feature-*', url: 'https://github.com/mesmeslip/test-X2.git']]])
+    checkout([$class: 'GitSCM', branches: [[name: '**']], 
+    doGenerateSubmoduleConfigurations: false, 
+    extensions: [], 
+    submoduleCfg: [], 
+    userRemoteConfigs: [[refspec: '+refs/heads/main:refs/remotes/origin/main +refs/heads/develop:refs/remotes/origin/develop +refs/heads/release-*:refs/remotes/origin/release-* +refs/heads/feature-*:refs/remotes/origin/feature-*', url: 'https://github.com/mesmeslip/test-X2.git']]])
     echo '''
         echo "test"
     '''

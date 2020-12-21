704437fcd65c565bd66c444c066654b3f76e480c
mesmeslip <49732550+mesmeslip@users.noreply.github.com>
2020-12-21 13:40:58 +0200

    test - 2

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,5 +6,7 @@ node {
     userRemoteConfigs: [[refspec: '+refs/heads/main:refs/remotes/origin/main +refs/heads/develop:refs/remotes/origin/develop +refs/heads/release-*:refs/remotes/origin/release-* +refs/heads/feature-*:refs/remotes/origin/feature-*', url: 'https://github.com/mesmeslip/test-X2.git']]])
     echo '''
         echo "test"
+        echo $payload
+        env
     '''
 }

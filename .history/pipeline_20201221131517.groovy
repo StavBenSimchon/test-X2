7d8299626b996162eb3b6392595433b843139390
mesmeslip <stv.bs101@gmail.com>
2021-03-31 18:58:00 +0300

    .

--- .history/pipeline_20201221131517.groovy
+++
@@ -1,6 +0,0 @@
-node {
-    checkout([$class: 'GitSCM', branches: [[name: '**']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[refspec: '+refs/heads/master:refs/remotes/origin/master +refs/heads/develop:refs/remotes/origin/develop +refs/heads/release-*:refs/remotes/origin/release-* +refs/heads/feature-*:refs/remotes/origin/feature-*', url: 'https://github.com/mesmeslip/test-X2.git']]])
-    echo '''
-        echo "test"
-    '''
-}
